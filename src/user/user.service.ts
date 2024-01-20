66f9c0a5342c239e481fb6d6b439dea9f5a5ffbc
ViktoryaSVA <ViktoryaSVA>
2024-01-20 18:49:41 +0200

    Feature-created methods for favorites articles

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -13,53 +13,56 @@ import { UpdateUserDto } from '@app/user/dto/updateUser.dto';
 export class UserService {
   constructor(
     @InjectRepository(UserEntity)
-    private readonly userRepository: Repository<UserEntity>
+    private readonly userRepository: Repository<UserEntity>,
   ) {}
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
     const userByEmail = await this.userRepository.findOne({
       where: {
-        email: createUserDto.email
-      }
+        email: createUserDto.email,
+      },
     });
     const userByUsername = await this.userRepository.findOne({
       where: {
-        username: createUserDto.username
-      }
+        username: createUserDto.username,
+      },
     });
 
     if (userByEmail || userByUsername) {
       throw new HttpException(
         'Email or username are taken',
-        HttpStatus.UNPROCESSABLE_ENTITY
+        HttpStatus.UNPROCESSABLE_ENTITY,
       );
     }
 
     const newUser = new UserEntity();
-    Object.assign(newUser, createUserDto)
+    Object.assign(newUser, createUserDto);
     return await this.userRepository.save(newUser);
   }
 
   async loginUser(loginUserDto: LoginUserDto): Promise<UserEntity> {
     const user = await this.userRepository.findOne({
       where: {
-        email: loginUserDto.email
+        email: loginUserDto.email,
       },
-      select: ['id', 'username', 'password', 'email', 'bio', 'image']
+      select: ['id', 'username', 'password', 'email', 'bio', 'image'],
     });
 
     if (!user) {
       throw new HttpException(
         'Credentials are not valid',
-        HttpStatus.UNPROCESSABLE_ENTITY
+        HttpStatus.UNPROCESSABLE_ENTITY,
       );
     }
 
-    const isPasswordCorrect = await compare(loginUserDto.password, user.password);
+    const isPasswordCorrect = await compare(
+      loginUserDto.password,
+      user.password,
+    );
 
     if (!isPasswordCorrect) {
       throw new HttpException(
         'Credentials are not valid',
-        HttpStatus.UNPROCESSABLE_ENTITY
+        HttpStatus.UNPROCESSABLE_ENTITY,
       );
     }
 
@@ -68,29 +71,34 @@ export class UserService {
     return user;
   }
 
-  async updateUser(userId: number, updateUserDto: UpdateUserDto): Promise<UserEntity> {
+  async updateUser(
+    userId: number,
+    updateUserDto: UpdateUserDto,
+  ): Promise<UserEntity> {
     const user = await this.findById(userId);
     Object.assign(user, updateUserDto);
     return await this.userRepository.save(user);
   }
 
   findById(id: number): Promise<UserEntity> {
-    return this.userRepository.findOne({ where: { id } } );
+    return this.userRepository.findOne({ where: { id } });
   }
   generateJWT(user: UserEntity): string {
-    return sign({
-      id: user.id,
-      username: user.username,
-      email: user.email
-    }, JWT_SECRET_KEY
+    return sign(
+      {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+      JWT_SECRET_KEY,
     );
   }
   buildUserResponse(user: UserEntity): UserResponseInterface {
     return {
       user: {
         ...user,
-        token: this.generateJWT(user)
-      }
-    }
+        token: this.generateJWT(user),
+      },
+    };
   }
 }
